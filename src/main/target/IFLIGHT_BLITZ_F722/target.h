2f776199b1e026a367d4a76174bb58342772bc91
kasatka60 <61105679+kasatka60@users.noreply.github.com>
2025-12-24 21:02:47 +0300

    Update target.h
    Добавил вторую шину I2C для компаса

--- src/main/target/IFLIGHT_BLITZ_F722/target.h
+++ src/main/target/IFLIGHT_BLITZ_F722/target.h
@@ -53,6 +53,10 @@
 #define I2C1_SCL                PB8
 #define I2C1_SDA                PB9
 
+#define USE_I2C_DEVICE_2
+#define I2C2_SCL                PB10
+#define I2C2_SDA                PB11
+
 #define USE_BARO
 #define BARO_I2C_BUS            BUS_I2C1
 #define USE_BARO_BMP280
@@ -60,15 +64,15 @@
 #define USE_BARO_DPS310
 
 #define USE_MAG
-#define MAG_I2C_BUS             BUS_I2C1
+#define MAG_I2C_BUS             BUS_I2C2
 #define USE_MAG_ALL
 
-#define TEMPERATURE_I2C_BUS     BUS_I2C1
+#define TEMPERATURE_I2C_BUS     BUS_I2C2
 
-#define PITOT_I2C_BUS           BUS_I2C1
+#define PITOT_I2C_BUS           BUS_I2C2
 
 #define USE_RANGEFINDER
-#define RANGEFINDER_I2C_BUS     BUS_I2C1
+#define RANGEFINDER_I2C_BUS     BUS_I2C2
 
 // *************** SPI2 OSD***********************
 #define USE_SPI_DEVICE_2
